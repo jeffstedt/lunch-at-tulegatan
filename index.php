919ac3a4ac3a233b1564225f81d2717af4f7e49e
Jeffrey Örnerstedt <jeffrey.kontakt@gmail.com>
2016-09-01 13:59:23 +0200

    Added more php logic and jQuery countdown plugin

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>När är det lunch?</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -19,8 +19,9 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Hello, lunch!</h1>
+    <h1>När är det lunch?</h1>
 
+    <div class="ajax">
     <?php
 
     // SET TIMEZONE
@@ -30,28 +31,36 @@
     $clock = date('H:i');
     $clock=date('H:i', strtotime($clock));;
 
-    echo "the clock is: ";
+    echo "the clock is: <b>";
     echo($clock);
-    echo "<br>";
+    echo "</b><br>";
 
-    echo "current weekday: ";
+    echo "current weekday: <b>";
     echo($weekday);
-    echo "<br>";
+    echo "</b><br>";
+
+    $currentDate = date('Y/m/d');
+    $currentDate = date('Y/m/d', strtotime($currentDate));;
+
 
         //DATE INTERVAL FOR COUNTDOWN
-        $timerStart = "09:00";
-        $timerEnd = "14:00";
+        $timerStart = "09:00:00";
+        $timerEnd   = "14:00:00";
 
         // DATE INTERVAL FOR LUNCH
-        $lunchStart = "11:00";
-        $lunchEnd = "15:00";
+        $lunchStart = "11:30:00";
+        $lunchEnd   = "14:00:00";
+
+      echo "Countdown target : <b>" . $currentDate . " " . $lunchEnd . "</b>";
+      echo "<br>";
 
           // If weekday is between 1 and 6 display COUNTDOWN
     	if ($weekday > 0 && $weekday < 6) {
 
               // If clock it between $timerStart and $timer display Google Api
           if  ( ($clock > $timerStart) && ($clock < $timerEnd) ) {
-              echo "<br> Show COUNTDOWN";
+              echo "<br> Show COUNTDOWN<br>";
+              echo '<div id="clock"></div>';
 
               // If clock is between $lunchStart and $lunchEnd disyplay Google Api
               if ( ($clock > $lunchStart) && ($clock < $lunchEnd) )
@@ -66,12 +75,24 @@
     	}
 
 
-
      ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/jquery.countdown.min.js"></script>
+
+    <script type="text/javascript">
+      $('#clock').countdown('<?php echo $currentDate . " " . $lunchStart; ?>', function(event) {
+      var $this = $(this).html(event.strftime(''
+      + '<span>%d</span> days '
+      + '<span>%H</span> hr '
+      + '<span>%M</span> min '
+      + '<span>%S</span> sec'));
+    });
+    </script>
+    </div> <!-- end of AJAX -->
   </body>
 </html>
